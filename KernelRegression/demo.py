cccc0a7025e345f1225c7f3eef897382eb995128
jmdbsa0012 <jmdbsa0012@gmail.com>
2017-09-06 15:50:30 -0400

    adding bank

--- KernelRegression/demo.py
+++ KernelRegression/demo.py
@@ -10,18 +10,50 @@ from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C, Matern
 
 ### hardware example:
 
-observation, trueLabel = pl.preproIRIS()
-noiseLabel = pl.blurringDis(trueLabel,0.1)
+# observation, trueLabel = pl.preproIRIS()
+# noiseLabel = pl.blurringDis(trueLabel,0.15)
 
-kernel =  Matern()
-cf = GPG.GPregression(kernelParameter=kernel)
+# print(trueLabel)
+# print(noiseLabel)
 
-shift = 5
-alert = cf.OnlineGP(observation,noiseLabel,shift=shift)
-print(alert)
-evaluation = cf.checking(alert,noiseLabel,trueLabel,shift=shift)
-name1 = 'test1GP.pdf'
-name2 = 'Streamtest1GP.pdf'
+
+### Wine example
+# observation, trueLabel = pl.preprowine()
+# noiseLabel = pl.blurringDis(trueLabel,0.15)
+
+### banke example
+observation, trueLabel = pl.preprobank()
+noiseLabel = pl.blurringDis(trueLabel,0.15)
+
+
+
+
+
+### adult example
+# observation, trueLabel = pl.preproAdult()
+# noiseLabel = pl.blurringDis(trueLabel,0.15)
+
+
+## GPC approach
+# kernel =  Matern()
+# cf = GPG.GPregression(kernelParameter=kernel)
+# shift = 40
+# alert = cf.OnlineGPC(observation[:500,:],noiseLabel[:500],shift=shift)
+# evaluation = cf.checking(alert,noiseLabel[:500],trueLabel[:500],shift=shift)
+# name1 = 'bankGPC.pdf'
+# name2 = 'bankProGPC.pdf'
+# cf.drawing(evaluation,name1)
+# cf.error_visu(evaluation,name2)
+
+
+## testing in logistic softmax regression
+shift = 40
+cf = GPG.LogisticRegression()
+
+alert = cf.logRe(observation[:500,:],noiseLabel[:500],shift=shift)
+evaluation = cf.checking(alert,noiseLabel[:500],trueLabel[:500],shift=shift)
+name1 = 'bankLog.pdf'
+name2 = 'bankProLog.pdf'
 cf.drawing(evaluation,name1)
 cf.error_visu(evaluation,name2)
 
